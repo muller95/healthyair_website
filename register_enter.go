b2f6a779c6ffa17be8b41efc4e9a578cd459048c
Vadim <muller95@yandex.ru>
2017-06-24 23:25:27 +0300

    Ru+En support

--- register_enter.go
+++ register_enter.go
@@ -7,9 +7,38 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/valyala/fasthttp"
+
+	"html/template"
 )
 
 //func send_to_mysql()
+
+type RegisterCard struct {
+	Password     string
+	Register     string
+	Registration string
+	Cancel       string
+	Name         string
+}
+
+func registerForm(ctx *fasthttp.RequestCtx, language string) {
+	resources := languageResources["en"]
+	if language == "ru" {
+		resources = languageResources["ru"]
+	}
+
+	card := RegisterCard{Password: resources["Password"], Register: resources["Register"],
+		Cancel: resources["Cancel"], Name: resources["Name"], Registration: resources["Registration"]}
+	registerTemplate, err := template.ParseFiles("public/views/register.html")
+	if err != nil {
+		log.Println("Err on parsing register card template: ", err)
+	}
+	ctx.SetContentType("text/html")
+	err = registerTemplate.Execute(ctx, card)
+	if err != nil {
+		log.Println("Err on executing register template: ", err)
+	}
+}
 
 func registerEnter(ctx *fasthttp.RequestCtx) {
 	// var err error
