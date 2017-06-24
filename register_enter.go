232f25ab9195b936bcb9378fc30cea7d9b8b82c2
Lavabar <user@debian>
2017-06-24 10:06:29 +0300

    fixing sql...

--- register_enter.go
+++ register_enter.go
@@ -20,20 +20,22 @@ func registerEnter(ctx *fasthttp.RequestCtx) {
 	if ctx.IsPost() {
 		pbody := string(ctx.PostBody())
 		fmt.Println(string(pbody))
-		//request must contains 2 args(email, password)
+		//request must contain 2 args(email, password)
 		if strings.Contains(pbody, "&") {
 			email = pbody[strings.Index(pbody, "=")+1 : strings.Index(pbody, "&")]
 			pbody = strings.TrimPrefix(pbody, pbody[:strings.Index(pbody, "&")+1])
 		}
 		passwd = pbody[strings.Index(pbody, "=")+1:]
-
-		_, err = dbConn.Prepare("INSERT INTO users (email, passwd) VALUES (?, ?)")
+		//stmt := fmt.Sprintf("INSERT INTO users (email, passwd) VALUES (\"%s\", \"%s\")", email, passwd)
+		_, err = dbConn.Prepare("INSERT INTO users (email, passwd) VALUES (\"?\", \"?\")")
 		if err != nil {
-			log.Fatal("@ERR ON PREPARING STMT: 'INSERT INTO users (email, passwd) VALUES (?, ?)'")
+			log.Fatal("@ERR ON PREPARING STMT: 'INSERT INTO users (email, passwd) VALUES (\"?\", \"?\")':", err)
 		}
+
+		//fmt.Println(stmt)
 		_, err = dbConn.Exec(email, passwd)
 		if err != nil {
-			log.Fatal("@ERR ON EXECUTING STMT: 'INSERT INTO users (email, passwd) VALUES (?, ?)'")
+			log.Fatal("@ERR ON EXECUTING STMT: 'INSERT INTO users (email, passwd) VALUES (?, ?)'", err)
 		}
 	} else {
 		log.Fatal("@ERR ON METHOD-REQUEST")
