e3b4b7792c82e4a826fe92dbb974357510df37d7
Vadim <muller95@yandex.ru>
2017-06-29 16:38:57 +0300

    Navbar as separete template

--- main_page.go
+++ main_page.go
@@ -4,31 +4,64 @@ import (
 	"html/template"
 	"log"
 
+	"bytes"
+
 	"github.com/valyala/fasthttp"
 )
 
-type MainPage struct {
-	MainPage string
+type Navbar struct {
 	Main     string
 	Buy      string
 	AboutUs  string
 	Contacts string
 }
 
+type MainPage struct {
+	MainPage string
+	Navbar   template.HTML
+}
+
+func executeNavbar(resources map[string]string) template.HTML {
+	var navbar Navbar
+
+	navbar.AboutUs = resources["AboutUs"]
+	navbar.Buy = resources["Buy"]
+	navbar.Contacts = resources["Contacts"]
+	navbar.Main = resources["Main"]
+
+	navbarTemplate, err := template.ParseFiles("public/views/nav_template.html")
+	if err != nil {
+		log.Println("Err on parsing main page template: ", err)
+		return ""
+	}
+
+	navbarWriter := &bytes.Buffer{}
+	err = navbarTemplate.Execute(navbarWriter, navbar)
+	if err != nil {
+		log.Println("Err on executing navbar template: ", err)
+		return ""
+	}
+
+	return template.HTML(navbarWriter.Bytes())
+}
+
 func mainPage(ctx *fasthttp.RequestCtx, language string) {
+	var mainPage MainPage
+
 	resources := languageResources["en"]
 	if language == "ru" {
 		resources = languageResources["ru"]
 	}
 
-	page := MainPage{MainPage: resources["MainPage"], Main: resources["Main"], Buy: resources["Buy"],
-		AboutUs: resources["AboutUs"], Contacts: resources["Contacts"]}
+	mainPage.MainPage = resources["MainPage"]
+	mainPage.Navbar = executeNavbar(resources)
+
 	registerTemplate, err := template.ParseFiles("public/views/main_page.html")
 	if err != nil {
 		log.Println("Err on parsing main page template: ", err)
 	}
 	ctx.SetContentType("text/html")
-	err = registerTemplate.Execute(ctx, page)
+	err = registerTemplate.Execute(ctx, mainPage)
 	if err != nil {
 		log.Println("Err on executing main page template: ", err)
 	}
