d33deae16cf09f779084501797de39c014314509
Vadim <muller95@yandex.ru>
2017-07-06 17:10:30 +0300

    *travis

--- travis-install.sh
+++ travis-install.sh
@@ -12,4 +12,4 @@ go get "github.com/go-sql-driver/mysql"
 go get "github.com/google/uuid"
 go get "github.com/tarantool/go-tarantool"
 
-sudo apt-get install -y mysql
+sudo apt-get install -y mysql-server
